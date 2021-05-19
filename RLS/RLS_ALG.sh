5ca9fc5efd45a43977f075cd092102b6d808810e
Ваше Имя <you@example.com>
2021-05-19 11:36:37 +0300

    Init commit

--- RLS/RLS_ALG.sh
+++ RLS/RLS_ALG.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 declare -a targets
+targetDirectory='/tmp/GenTargets/Targets'
+targetsNames=`ls -t $targetDirectory`
+echo $targetsNames
+# for (( i = 0; i < 30; i++ ))
+# do
+# 	targets[i]=
+# done
